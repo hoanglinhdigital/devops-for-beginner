151d4eac4c1ed8b0686ded55834564d026bbddc0
nvlinh6 <nvlinh6@HCMLM-nvlinh6.local>
2024-02-07 10:16:02 +0700

    Modify lab6.goovy after run successfully

--- cicd-using-jenkins/lab6/lab6.groovy
+++ cicd-using-jenkins/lab6/lab6.groovy
@@ -12,11 +12,10 @@ pipeline {
         stage('Update task definition and force deploy ecs service') {
             steps {
                 sh '''
-                    echo "New image: ${FULL_IMAGE}"
                     TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition ${TASK_FAMILY} --region "ap-southeast-1")
                     NEW_TASK_DEFINITION=$(echo $TASK_DEFINITION | jq --arg IMAGE "${FULL_IMAGE}" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities) |  del(.registeredAt)  | del(.registeredBy)')
                     NEW_TASK_INFO=$(aws ecs register-task-definition --region "ap-southeast-1" --cli-input-json "$NEW_TASK_DEFINITION")
-                    NEW_REVISION=$(echo $NEW_TASK_INFO | jq '.taskDefinition.revision')'
+                    NEW_REVISION=$(echo $NEW_TASK_INFO | jq '.taskDefinition.revision')
                     aws ecs update-service --cluster udemy-devops-ecs-cluster --service nodejs-service --task-definition ${TASK_FAMILY}:${NEW_REVISION} --force-new-deployment
                 '''
  
