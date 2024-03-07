4b55a6ca515e04b37be07a88afffcff342fefb7b
nvlinh6 <nvlinh6@HCMLM-nvlinh6.local>
2024-03-07 14:59:17 +0700

    add KOPS lab

--- kubernetes-basic/lab12-kops/lab12.sh
+++ kubernetes-basic/lab12-kops/lab12.sh
@@ -4,14 +4,33 @@
 #Sử dụng Brew dể cài Kops và Kubectl
 brew update && brew install kops kubectl
 
-aws configure
+#cấp cho Kops instance quyền iam role có policy: AdministratorAccess.
+
+#Cài đặt Kubectl & Kops cho server.
+#Tham khảo link sau:
+#https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl help
+
+#Cài đặt Kops
+#Tham khảo link sau: https://kops.sigs.k8s.io/getting_started/install/
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
+kops version
+
 
 #Tạo một S3 bucket để lưu trữ state của Kops
-aws s3api create-bucket --bucket kops-state-store-linh --region ap-southeast-1
+aws s3api create-bucket --bucket udemy-devops-kops --region ap-southeast-1
 
-export KOPS_CLUSTER_NAME=devops.cluster.demo
-export KOPS_STATE_STORE=s3://kops-state-store-linh
+#Tạo cluster = KOPS command
+kops create cluster --name=kubevpro.groophy.in \ 
+--state=s3://udemy-devops-kops --zones=ap-southeast-1a,ap-southeast-1c \ 
+--node-count=2 --node-size=t3.small --master-size=t3.small --dns-zone=hoanglinhdigital.com \ 
+--node-volume-size=8 --master-volume-size=8
 
-kops create cluster --zones ap-southeast-1a,ap-southeast-1c ${KOPS_CLUSTER_NAME}
+kops update cluster --name kubevpro.groophy.in --state=s3://udemy-devops-kops --yes --admin
 
-kops update cluster --name ${KOPS_CLUSTER_NAME} --yes
+kops delete cluster --name kubevpro.groophy.in --state=s3://udemy-devops-kops --yes --admin
