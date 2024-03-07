da5abc52c6c5e9a52eeb1173b47bcfeb50244c53
hoanglinhdigital <hoanglinhdigital@gmail.com>
2024-03-07 21:39:23 +0700

    Modify ater lab 12 kops

--- kubernetes-basic/lab12-kops/lab12.sh
+++ kubernetes-basic/lab12-kops/lab12.sh
@@ -1,9 +1,9 @@
 #Tạo một EC2 chạy Ubuntu
 
-#Cài đặt Brew
-#Sử dụng Brew dể cài Kops và Kubectl
-brew update && brew install kops kubectl
-
+#Cài đặt AWS CLI:
+sudo apt update
+sudo apt install awscli -y
+aws --version
 #cấp cho Kops instance quyền iam role có policy: AdministratorAccess.
 
 #Cài đặt Kubectl & Kops cho server.
@@ -21,16 +21,43 @@ chmod +x kops
 sudo mv kops /usr/local/bin/kops
 kops version
 
+#Chuẩn bị domain, có thể mua tại Route53.
+#Kiểm tra domain:
+nslookup -type=ns hoanglinhdigital.com
 
 #Tạo một S3 bucket để lưu trữ state của Kops
 aws s3api create-bucket --bucket udemy-devops-kops --region ap-southeast-1
 
 #Tạo cluster = KOPS command
-kops create cluster --name=kubevpro.groophy.in \ 
---state=s3://udemy-devops-kops --zones=ap-southeast-1a,ap-southeast-1c \ 
---node-count=2 --node-size=t3.small --master-size=t3.small --dns-zone=hoanglinhdigital.com \ 
---node-volume-size=8 --master-volume-size=8
+kops create cluster --name hoanglinhdigital.com --state=s3://udemy-devops-kops --zones=ap-southeast-1a,ap-southeast-1c --node-count=2 --node-size=t3.small --master-size=t3.small --dns-zone hoanglinhdigital.com --node-volume-size=10 --master-volume-size=10
 
-kops update cluster --name kubevpro.groophy.in --state=s3://udemy-devops-kops --yes --admin
+#Apply thay đổi thực sự:
+kops update cluster --name hoanglinhdigital.com --state=s3://udemy-devops-kops --yes --admin
 
-kops delete cluster --name kubevpro.groophy.in --state=s3://udemy-devops-kops --yes --admin
+#Validate cluster:
+kops validate cluster --name hoanglinhdigital.com --state=s3://udemy-devops-kops --wait 10m
+
+#getnode:
+kubectl get nodes
+
+#output as below is OK
+NODE STATUS
+NAME                    ROLE            READY
+i-03ee28b8c4c2fd0cb     node            True
+i-04b3c3d35f590fc60     control-plane   True
+i-0cdc0ec740ecf6504     node            True
+
+#kiểm tra Kubenetes config file được tạo ra:
+cat ~/.kube/config
+
+#Copy các item trong config và paste vào file ~/.kube/config trên máy local.
+#Đối với Windows: C:\Users\{username}\.kube\config
+
+#Kiểm tra thông tin cluster:
+kubectl cluster-info
+kubectl get nodes
+
+
+
+#LƯU Ý: Xoá Cluster nếu không sử dụng đến để tránh tốn phí:
+kops delete cluster --name hoanglinhdigital.com --state=s3://udemy-devops-kops --yes --admin
---
+++ kubernetes-basic/lab13-deploy-resources-to-kubernetes/lab13.sh
@@ -0,0 +1,8 @@
+#Yêu cầu: Đã làm xong bài lab số 12, tạo Cluster thành công.
+#Kiểm tra một lần nữa trạng thái của Cluster:
+kubectl cluster-info
+kubectl get nodes
+
+#Tạo một file lab-13.yaml như mẫu:
+
+
