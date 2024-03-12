ac688f2531ba30264d7fcb564a7e0610d0cadd5f
hoanglinhdigital <hoanglinhdigital@gmail.com>
2024-03-12 22:12:06 +0700

    Modify after lab13

--- kubernetes-basic/lab13-deploy-resources-to-kubernetes/lab13.sh
+++ kubernetes-basic/lab13-deploy-resources-to-kubernetes/lab13.sh
@@ -14,38 +14,45 @@ kops export kubecfg --name=hoanglinhdigital.com --state=s3://udemy-devops-kops
 https://github.com/docker/awesome-compose
 #Repository bao gồm rất nhiều bài lab, ở đây mình chọn bài lab trong thư mục này để hướng dẫn các bạn:
 react-express-mongodb
-
 #*Repository backup cho trường hợp repo bên trên bị xóa:
-https://github.com/hoanglinhdigital/awesome-compose
+https://github.com/hoanglinhdigital/react-express-mongodb
 
-#Step2: Chạy thử bằng lệnh Docker Compose
+
+#===Step2: Chạy thử bằng lệnh Docker Compose
 cd react-express-mongodb
-docker-compose up -d
+docker-compose -f compose.yaml up -d
 #Kiểm tra trạng thái các container:
 docker-compose ps
 #Truy cập thử vào localhost:3000
 #Nếu chạy OK ta mới tiến hành step tiếp theo.
 
-#===Step3: Tạo một repository trong GitHub, tên là react-express-mongodb
-#Clone repository về máy:
-#Copy toàn bộ nội dung của thư mục react-express-mongodb vào thư mục vừa clone về.
-#Push lên GitHub.
-
-#===Step4: Build các Docker image và push lên ECR.
+#===Step3: Build các Docker image và push lên ECR.
 # - Tạo các ECR repositories cho các image
 # - Build các image và push lên ECR
+#LƯU Ý cho phần frontend, các bạn chỉnh sửa file package.json và xoá dòng proxy để tránh chạy lỗi trên Kubernetes.
+"proxy": "http://backend:3000",
 
-#===Step5: Tạo các file kubernetes config cho các resource
+
+#===Step4: Tạo các file kubernetes config cho các resource
 # - Tạo file kubernetes config cho React-Backend
 # - Tạo file kubernetes config cho Express-Frontend
 # - Tạo file kubernetes config cho MongoDB
-# - Tạo file kubernetes config cho LoadBalancer (Sử dụng ALB Ingress Controller)
+# - Tạo file kubernetes config cho Ingress Nginx với 2 route /api -> backend và / -> frontend
 
-#===Step6: Deploy các resource lên Kubernetes theo thứ tự sau:
-# - Deploy MongoDB
-# - Deploy Express-Backend
-# - Deploy React-Frontend
-# - Deploy LoadBalancer 
+#===Step5: Tạo Ingress NGinx Controller bằng cách tham khảo guide sau:
+https://github.com/kubernetes/ingress-nginx/blob/main/docs%2Fdeploy%2Findex.md
+#Keyword: Bare metal clusters
+#Hoặc các bạn cũng có thể copy lệnh sau chạy luôn:
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.0/deploy/static/provider/baremetal/deploy.yaml
+#Kiểm tra trạng thái của Ingress Nginx Controller
+kubectl get services -n ingress-nginx
+#Kết quả như sau là OK:
+NAME                                 TYPE        CLUSTER-IP       EXTERNAL-IP   PORT(S)                      AGE
+ingress-nginx-controller             NodePort    100.67.79.127    <none>        80:31646/TCP,443:31609/TCP   16s
+ingress-nginx-controller-admission   ClusterIP   100.71.229.247   <none>        443/TCP                      15s
+
+#===Step6: Deploy các resource lên Kubernetes
+kubectl apply -f lab13.yaml
 
 #===Step7: Kiểm tra trạng thái của các resource
 # - Kiểm tra trạng thái của các Pod
@@ -56,49 +63,22 @@ kubectl get services
 kubectl get deployments
 # - Kiểm tra trạng thái của các Ingress
 kubectl get ingress
+kubectl describe ingress/ingress-nginx
 
-#===Step8: Truy cập vào ứng dụng thông qua LoadBalancer DNS
+#===Step8: Tạo LoadBalancer + Target Group
+#Tạo một alb-security-group, rule HTTP:80 cho phép truy cập từ mọi nơi.
+#Tạo một APplication Load Balancer, listerner port 80
+#Tạo một target group type Instance, port HTTP:30927 (*số 30297 lấy từ câu lệnh kubectl "get services -n ingress-nginx")
+#Cấu hình security group của các instance cho phép truy cập từ alb-security-group (All traffic).
+#Truy cập vào Load Balancer thông qua DNS, test ứng dụng.
 
 #===Step9: Xoá các resource
 # - Xoá LoadBalancer
-# - Xoá React-Frontend
-# - Xoá Express-Backend
-# - Xoá MongoDB
+# - Xoá Target Group
+# - Xoá các resource trên Kubernetes bằng lệnh:
+kubectl delete -f lab13.yaml
 
-#===Step10: Xoá Cluster
+#===Step10: Xoá Cluster (chạy lệnh này trong KOPS admin server)
 kops delete cluster --name hoanglinhdigital.com --state=s3://udemy-devops-kops --yes
 
-
-
-
-#Note 2024-03-12 - Create AWS Load Balancer controller.
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=aws-load-balancer-webhook-service.kube-system.svc"
-kubectl create secret generic aws-load-balancer-webhook-tls --from-file=tls.crt --from-file=tls.key -n kube-system
-kubectl get secret aws-load-balancer-webhook-tls -n kube-system
-
-# Kubernetes >= 1.15
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.2.0/docs/install/v2_2_0_full.yaml
-
-# Kubernetes < 1.15
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.2.0/docs/install/v2_2_0_full_legacy.yaml
-
-#Verify the deployment of the AWS Load Balancer Controller
-kubectl get deployment -n kube-system aws-load-balancer-controller
-
-
-##
-helm install aws-load-balancer-controller eks/aws-load-balancer-controller --set clusterName=hoanglinhdigital.com -n kube-system
-kubectl get deployment -n kube-system aws-load-balancer-controller
-
-kubectl get deployment aws-load-balancer-controller -o yaml -n kube-system > test-2.yaml
-#Modify test-2.yaml
-#Add below lines:
-      containers:
-      - args:
-        - --cluster-name=my-hoanglinhdigital.com
-        - --ingress-class=alb
-        - --aws-region=ap-southeast-1
-        - --aws-vpc-id=vpc-005bcc5f352b57f1d
-
-
-
+#===Step11: Xoá Kops server hoặc stop nếu muốn sử dụng lại.
