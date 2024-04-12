c2b10e7f4584e5374953b02d8c74643503565e15
hoanglinhdigital <hoanglinhdigital@gmail.com>
2024-04-12 21:19:02 +0700

    Fix express not found error of chapter Docker  lab5, lab6

--- docker-basic-operation/lab5-docker-compose/Command.sh
+++ docker-basic-operation/lab5-docker-compose/Command.sh
@@ -1,5 +1,13 @@
 #Start docker compose
 docker-compose -f docker-compose.yaml up -d
+#OR
+#Start and rebuild all docker images
+docker-compose -f docker-compose.yaml up -d --build
+
+#Check container:
+docker ps -a
+#OR
+docker-composse ps -a
 #Terminate all resource
 docker-compose -f docker-compose.yaml down
 
--- docker-basic-operation/lab6-docker-compose-multi/Command.sh
+++ docker-basic-operation/lab6-docker-compose-multi/Command.sh
@@ -1,6 +1,14 @@
 #Lab6 - Docker compose multi container
 #Start docker compose
 docker-compose -f docker-compose.yaml up -d
+#OR
+#Start and rebuild all docker images
+docker-compose -f docker-compose.yaml up -d --build
+
+#Check container:
+docker ps -a
+#OR
+docker-composse ps -a
 #Terminate all resource
 docker-compose -f docker-compose.yaml down
 
